ef623761fa7c9a3211ae39f6d37f290f8658e057
Zoltan Mathe <zoltan.mathe@cern.ch>
2013-05-02 17:52:53 +0200

    LHCb extension is being implemented which based on DIRAC

---
+++ WebApp/static/LHCbDIRAC/LHCbJobMonitor/classes/LHCbJobMonitor.js
@@ -0,0 +1,11 @@
+/*!
+ * Ext JS Library 4.0
+ * Copyright(c) 2006-2011 Sencha Inc.
+ * licensing@sencha.com
+ * http://www.sencha.com/license
+ */
+
+Ext.define('LHCbDIRAC.LHCbJobMonitor.classes.LHCbJobMonitor',{
+    extend:'DIRAC.JobMonitor.classes.JobMonitor',
+});
+
