46db7e109fe90ec735e19809033f0d3740aaf463
Adrià Casajús <adriancasajus@gmail.com>
2013-07-15 16:41:06 +0200

    Properly treat anon username and group. Fixes #62

--- WebApp/handler/AccountingPlotHandler.py
+++ WebApp/handler/AccountingPlotHandler.py
@@ -23,8 +23,10 @@ class AccountingPlotHandler(WebHandler):
 
   def __getUniqueKeyValues( self, typeName ):
     sessionData = SessionData().getData()
-    userGroup = sessionData["user"]["group"]
-    cacheKey = ( sessionData["user"]["username"], userGroup, sessionData["setup"], typeName )
+    cacheKey = ( sessionData["user"].get( "username", "" ),
+                 sessionData["user"].get( "group", "" ),
+                 sessionData["setup"],
+                 typeName )
     data = AccountingPlotHandler.__keysCache.get( cacheKey )
     if not data:
       rpcClient = RPCClient( "Accounting/ReportGenerator" )
