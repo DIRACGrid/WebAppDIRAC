cc87539886dcd13496591b814f6875b7dea03ffe
Zoltan Mathe <zoltan.mathe@cern.ch>
2014-02-19 17:09:27 +0100

    PilotSummary has implemented...

--- WebApp/static/DIRAC/PilotSummary/classes/PilotSummary.js
+++ WebApp/static/DIRAC/PilotSummary/classes/PilotSummary.js
@@ -3,7 +3,8 @@
  */
 Ext.define("DIRAC.PilotSummary.classes.PilotSummary", {
   extend : 'Ext.dirac.core.Module',
-  requires : ["Ext.dirac.utils.DiracBaseSelector", "Ext.dirac.utils.DiracJsonStore", "Ext.dirac.utils.DiracAjaxProxy", "Ext.dirac.utils.DiracPagingToolbar", 'Ext.dirac.utils.DiracToolButton', "Ext.dirac.utils.DiracApplicationContextMenu", "Ext.dirac.utils.DiracGridPanel"],
+  requires : ["Ext.dirac.utils.DiracBaseSelector", "Ext.dirac.utils.DiracJsonStore", "Ext.dirac.utils.DiracAjaxProxy", "Ext.dirac.utils.DiracPagingToolbar", 'Ext.dirac.utils.DiracToolButton', "Ext.dirac.utils.DiracApplicationContextMenu", "Ext.dirac.utils.DiracGridPanel", "Ext.dirac.utils.DiracRowExpander"],
+  applicationsToOpen : {'PilotMonitor':'DIRAC.PilotMonitor.classes.PilotMonitor'},
   loadState : function(data) {
     var me = this;
 
@@ -115,6 +116,9 @@ Ext.define("DIRAC.PilotSummary.classes.PilotSummary", {
           scope : me,
           cmbSelectors : selectors,
           datamap : map,
+          hasTimeSearchPanel : true, // TODO This has to be changed! I have to
+          // fix TypeError: me.getTimeSearch(...) is
+          // null in DiracBaseSelector line 497
           url : "PilotSummary/getSelectionData"
         });
 
@@ -139,6 +143,7 @@ Ext.define("DIRAC.PilotSummary.classes.PilotSummary", {
           scope : me
         });
     me.expSites = [];
+
     var expSite = function(value, xxx, obj) {
       if (value == 'Multiple') {
         var site = 'Empty value'
@@ -148,33 +153,28 @@ Ext.define("DIRAC.PilotSummary.classes.PilotSummary", {
           me.expSites[me.expSites.length] = [site, recIndex];
         } catch (e) {
         }
-        var html = '<img id="img.' + site + '" style="cursor: pointer; cursor: hand;" src="static/core/img/common/plus.gif"';
-        html = html + ' onclick="addEntries(\'' + site + '\',' + recIndex + ')" />';
+        var html = '<img id="img.' + site + '" style="cursor: pointer; cursor: hand;" src="static/core/img/icons/common/plus.gif"';
+        html = html + ' onclick="this.addEntries(\'' + site + '\',' + recIndex + ')" />';
         return html
       }
     }
+
+    /*
+     * var oColumns = { "None1" : { "dataIndex" : "CE", // TODO two None can not
+     * have in a dictionary key. // i.containes('None') DiracGridPanel.js line
+     * 191. "properties" : { width : 26, sortable : false, hideable : false,
+     * fixed : true, menuDisabled : true, hidable : false, name : 'expand', id :
+     * 'expand' }, renderer : expSite },
+     */
     var oColumns = {
-      "None" : {
-        "dataIndex" : "CE",
-        "properties" : {
-          width : 26,
-          sortable : false,
-          hideable : false,
-          fixed : true,
-          menuDisabled : true
-        },
-        renderer : expSite
-      },
-      "None" : {
+      "None2" : {
         "dataIndex" : "StatusIcon",
         "properties" : {
           width : 26,
           sortable : false,
           hideable : false,
           fixed : true,
-          menuDisabled : true,
-          name : 'expand',
-          id : 'expand'
+          menuDisabled : true
         },
         "renderFunction" : "rendererStatus"
       },
@@ -238,15 +238,14 @@ Ext.define("DIRAC.PilotSummary.classes.PilotSummary", {
     };
 
     var showPilothandler = function() {
-      var oId = GLOBAL.APP.CF.getFieldValueFromSelectedRow(me.grid, "JobID");
-      var sId = GLOBAL.APP.CF.zfill(oId, 8);
+      var oSite = GLOBAL.APP.CF.getFieldValueFromSelectedRow(me.grid, "Site");
       var setupdata = {};
       setupdata.data = {};
-      setupdata.currentState = sId;
+      setupdata.currentState = oSite;
       setupdata.data.leftMenu = {};
       setupdata.data.leftMenu.selectors = {};
-      setupdata.data.leftMenu.selectors.jobGroup = {
-        data_selected : [sId],
+      setupdata.data.leftMenu.selectors.site = {
+        data_selected : [oSite],
         hidden : false,
         not_selected : false
       };
@@ -275,17 +274,156 @@ Ext.define("DIRAC.PilotSummary.classes.PilotSummary", {
 
     me.grid = Ext.create('Ext.dirac.utils.DiracGridPanel', {
           store : me.dataStore,
-          features : [{
-            ftype : 'grouping'
-          }],
+          columnLines : true,
+          enableLocking : true,
+          width : 600,
+          height : 300,
           oColumns : oColumns,
           tbar : pagingToolbar,
           contextMenu : me.contextGridMenu,
           pagingToolbar : pagingToolbar,
-          scope : me
+          scope : me,
+          columnLines : true,
+          enableLocking : true,
+          plugins : [{
+            ptype : 'diracrowexpander',
+            checkField : {
+              'CE' : 'Multiple'
+            },
+            rowBodyTpl : ['<div id="expanded-Grid-{Site}"> </div>']
+          }],
+          viewConfig : { //TODO This code must go to the Ext.dirac.utils.DiracGridPanel class. This is used to have the same size of the columns...
+            listeners : {
+              refresh : function(dataview) {
+                var nodes = dataview.getNodes();
+                for (var i = 0; i < nodes.length; i++) {
+                  row = Ext.fly(nodes[i], '_rowExpander');
+                  row.setHeight(26);
+                }
+              }
+            }
+          }
+        });
+
+    me.grid.view.on('expandbody', function(rowNode, record, expandbody) {
+          var targetId = 'expanded-Grid-' + record.get('Site');
+          if (Ext.getCmp(targetId + "_grid") == null) {
+            var params = {
+              "expand" : Ext.JSON.encode([record.data.Site])
+            };
+            var oProxy = Ext.create('Ext.dirac.utils.DiracAjaxProxy', {
+                  url : GLOBAL.BASE_URL + 'PilotSummary/getPilotSummaryData'
+                });
+            oProxy.extraParams = params;
+            var expandStore = Ext.create("Ext.dirac.utils.DiracJsonStore", {
+                  proxy : oProxy,
+                  fields : me.dataFields,
+                  scope : me
+                });
+            expandStore.load();
+            var expandedGridPanel = Ext.create('Ext.grid.Panel', {
+                  forceFit : true,
+                  renderTo : targetId,
+                  id : targetId + "_grid",
+                  store : expandStore,
+                  columns : [{
+                    header : 'Site',
+                    sortable : false,
+                    dataIndex : 'Site',
+                    align : 'left',
+                    hideable : false,
+                    fixed : true
+                  }, {
+                    header : 'CE',
+                    sortable : false,
+                    dataIndex : 'CE',
+                    align : 'left',
+                    hideable : false,
+                    fixed : true
+                  }, {
+                    header : 'Status',
+                    width : 60,
+                    sortable : false,
+                    dataIndex : 'Status',
+                    align : 'left'
+                  }, {
+                    header : 'PilotJobEff (%)',
+                    sortable : false,
+                    dataIndex : 'PilotJobEff',
+                    align : 'left'
+                  }, {
+                    header : 'PilotsPerJob',
+                    sortable : false,
+                    dataIndex : 'PilotsPerJob',
+                    align : 'left'
+                  }, {
+                    header : 'Submitted',
+                    sortable : false,
+                    dataIndex : 'Submitted',
+                    align : 'left',
+                    hidden : true
+                  }, {
+                    header : 'Ready',
+                    sortable : false,
+                    dataIndex : 'Ready',
+                    align : 'left',
+                    hidden : true
+                  }, {
+                    header : 'Waiting',
+                    sortable : false,
+                    dataIndex : 'Waiting',
+                    align : 'left'
+                  }, {
+                    header : 'Scheduled',
+                    sortable : false,
+                    dataIndex : 'Scheduled',
+                    align : 'left'
+                  }, {
+                    header : 'Running',
+                    sortable : false,
+                    dataIndex : 'Running',
+                    align : 'left'
+                  }, {
+                    header : 'Done',
+                    sortable : false,
+                    dataIndex : 'Done',
+                    align : 'left'
+                  }, {
+                    header : 'Aborted',
+                    sortable : false,
+                    dataIndex : 'Aborted',
+                    align : 'left'
+                  }, {
+                    header : 'Aborted_Hour',
+                    sortable : false,
+                    dataIndex : 'Aborted_Hour',
+                    align : 'left'
+                  }, {
+                    header : 'Done_Empty',
+                    sortable : false,
+                    dataIndex : 'Done_Empty',
+                    align : 'left',
+                    hidden : true
+                  }, {
+                    header : 'Total',
+                    sortable : false,
+                    dataIndex : 'Total',
+                    align : 'left',
+                    hidden : true
+                  }]
+                });
+            rowNode.grid = expandedGridPanel;
+            expandedGridPanel.getEl().swallowEvent(['mouseover', 'mousedown', 'click', 'dblclick', 'onRowFocus']);
+            expandedGridPanel.fireEvent("bind", expandedGridPanel, {
+                  id : record.get('id')
+                });
+          }
         });
 
     me.add([me.leftPanel, me.grid]);
 
+  },
+  addEntries : function(site, recordIndex) {
+    alert('Cool');
   }
 });
