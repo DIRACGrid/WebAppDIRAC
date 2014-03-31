b34b1856d555d868b36e9eb6605bd83675443604
Zoltan Mathe <zoltan.mathe@cern.ch>
2014-03-31 12:14:42 +0200

    comments are added

--- Lib/CompilerHelper.py
+++ Lib/CompilerHelper.py
@@ -1,10 +1,22 @@
-from DIRAC import gConfig
+# $HeadURL:  $
+""" CompilerHelper
+
+  It is a helper used to compile the applications
+
+"""
+
+from DIRAC import gConfig, gLogger
 from WebAppDIRAC.Lib import Conf
 from WebAppDIRAC.Core.App import App
 
 class CompilerHelper:
-  
+  '''
+  This class parse the cfg files and discovers the applications dependencies.
+  '''
   def __init__(self):
+    '''
+    It instantiate the application main class in order to load the configuration files.
+    '''
     app = App()
     app._loadWebAppCFGFiles()
     self.__dependencySection = "Dependencies"
@@ -13,11 +25,13 @@ class CompilerHelper:
   def getAppDependencies(self):
     """
     Generate the dependency dictionary
+    :return: Dict
     """
     dependency = {}
     fullName = "%s/%s" % ( Conf.BASECS, self.__dependencySection)
     result = gConfig.getOptions( fullName )
     if not result[ 'OK' ]:
+      gLogger.error(result['Message'])
       return dependency
     optionsList = result[ 'Value' ]
     for opName in optionsList:
