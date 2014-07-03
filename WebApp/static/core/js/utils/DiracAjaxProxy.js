5bad6288c42e75dec882459f67794c20f59991b0
Zoltan Mathe <zoltan.mathe@cern.ch>
2014-07-03 11:12:27 +0200

    use the correct error logging...

--- WebApp/static/core/js/utils/DiracAjaxProxy.js
+++ WebApp/static/core/js/utils/DiracAjaxProxy.js
@@ -40,10 +40,7 @@ Ext.define('Ext.dirac.utils.DiracAjaxProxy', {
       dontLoadOnCreation : false,
       listeners : {
         exception : function(proxy, response, operation) {
-          var jsonData = Ext.JSON.decode(response.responseText);
-          if (jsonData && jsonData["success"] == "false") {
-            GLOBAL.APP.CF.alert(jsonData["error"], "info");
-          }
+          GLOBAL.APP.CF.showAjaxErrorMessage(response);
         }
       }
     });
