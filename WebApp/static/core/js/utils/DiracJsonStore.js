4640694f2e2b48796a46062f7bf5e755f12ffb26
Zoltan Mathe <zoltan.mathe@cern.ch>
2014-12-09 17:22:30 +0100

    check the existence of the date

--- WebApp/static/core/js/utils/DiracJsonStore.js
+++ WebApp/static/core/js/utils/DiracJsonStore.js
@@ -108,26 +108,29 @@ Ext.define('Ext.dirac.utils.DiracJsonStore', {
           } else {
             if (oStore.proxy.reader.rawData && me.scope.grid && me.scope.grid.pagingToolbar) {
               var utcTime = null;
-              var newDate = Ext.Date.parse(Ext.String.trim(oStore.proxy.reader.rawData["date"].split("[UTC")[0]), "Y-m-d H:i");
-              if (newDate) {
-                if (me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp) {
-                  var msMinute = 60 * 1000, msDay = 60 * 60 * 24 * 1000, msHour = 60 * 60 * 1000;
-                  var days = Math.floor((newDate - me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp) / msDay);
-                  var hours = Math.floor(((newDate - me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp) % msDay) / msHour);
-                  var minutes = Math.floor((((newDate - me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp) % msDay) % msHour) / msMinute);
+              if (oStore.proxy.reader.rawData["date"]) {
+                var newDate = Ext.Date.parse(Ext.String.trim(oStore.proxy.reader.rawData["date"].split("[UTC")[0]), "Y-m-d H:i");
+                if (newDate) {
+                  if (me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp) {
+                    var msMinute = 60 * 1000, msDay = 60 * 60 * 24 * 1000, msHour = 60 * 60 * 1000;
+                    var days = Math.floor((newDate - me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp) / msDay);
+                    var hours = Math.floor(((newDate - me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp) % msDay) / msHour);
+                    var minutes = Math.floor((((newDate - me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp) % msDay) % msHour) / msMinute);
 
-                  utcTime = days + " " + hours + ":" + minutes;
+                    utcTime = days + " " + hours + ":" + minutes;
 
-                } else {
-                  me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp = newDate;
+                  } else {
+                    me.scope.grid.pagingToolbar.updateStamp.updateTimeStamp = newDate;
+                  }
+
                 }
+                if (utcTime) {
+                  me.scope.grid.pagingToolbar.updateStamp.setText('Updated: ' + oStore.proxy.reader.rawData["date"] + "(" + utcTime + ")");
+                } else {
+                  me.scope.grid.pagingToolbar.updateStamp.setText('Updated: ' + oStore.proxy.reader.rawData["date"]);
+                }
+              }
 
-              }
-              if (utcTime) {
-                me.scope.grid.pagingToolbar.updateStamp.setText('Updated: ' + oStore.proxy.reader.rawData["date"] + "(" + utcTime + ")");
-              } else {
-                me.scope.grid.pagingToolbar.updateStamp.setText('Updated: ' + oStore.proxy.reader.rawData["date"]);
-              }
             }
 
             me.remoteSort = false;
