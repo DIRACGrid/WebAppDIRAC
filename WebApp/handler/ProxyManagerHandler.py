6a3e66ab70738bc1eaf54a9a2ad562943e9a9444
Zoltan Mathe <zoltan.mathe@cern.ch>
2015-01-16 14:59:29 +0100

    Proxy manager application is implemented

---
+++ WebApp/handler/ProxyManagerHandler.py
@@ -0,0 +1,187 @@
+import base64
+import zlib
+import json
+from WebAppDIRAC.Lib.WebHandler import WebHandler, WErr, WOK, asyncGen
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.Core.Utilities.List import uniqueElements
+from DIRAC import gConfig, gLogger
+from DIRAC.Core.Utilities import Time
+
+class ProxyManagerHandler( WebHandler ):
+
+  AUTH_PROPS = "authenticated"
+  
+  @asyncGen
+  def web_getSelectionData( self ):
+    
+    sData = self.getSessionData()
+    
+    callback = {}
+
+    user = sData["user"]["username"]
+    if user == "Anonymous":
+      self.finish( {"success":"false", "error":"You are not authorize to access these data"} )
+      
+    if len( self.request.arguments ) > 0:
+      tmp = {}
+      for i in self.request.arguments:
+        tmp[i] = str( self.request.arguments[i] )
+      callback["extra"] = tmp
+    rpcClient = RPCClient( "Framework/ProxyManager" )
+    retVal = yield self.threadTask( rpcClient.getContents, {}, [], 0, 0 )
+    if not retVal[ "OK" ]:
+      self.finish( {"success":"false", "error":retVal["Message"]} )
+    data = retVal[ "Value" ]
+    users = []
+    groups = []
+    for record in data[ "Records" ]:
+      users.append( str( record[0] ) )
+      groups.append( str( record[2] ) )
+    users = uniqueElements( users )
+    groups = uniqueElements( groups )
+    users.sort()
+    groups.sort()
+    users = map( lambda x: [x], users )
+    groups = map( lambda x: [x], groups )
+    if len( users ) > 1:
+      users.insert( 0, ["All"] )
+    if len( groups ) > 1:
+      groups.insert( 0, ["All"] )
+    callback["username"] = users
+    callback["usergroup"] = groups
+    result = gConfig.getOption( "/Website/ProxyManagementMonitoring/TimeSpan" )
+    if result["OK"]:
+      tmp = result["Value"]
+      tmp = tmp.split( ", " )
+      if len( tmp ) > 0:
+        timespan = []
+        for i in tmp:
+          human_readable = self.__humanize_time( i )
+          timespan.append( [i, human_readable] )
+      else:
+        timespan = [["Nothing to display"]]
+    else:
+      timespan = [["Error during RPC call"]]
+    callback["expiredBefore"] = timespan
+    callback["expiredAfter"] = timespan
+    self.finish( callback )
+  
+  @asyncGen
+  def web_getProxyManagerData( self ):
+    sData = self.getSessionData()
+    
+    callback = {}
+
+    user = sData["user"]["username"]
+    if user == "Anonymous":
+      self.finish( {"success":"false", "error":"You are not authorize to access these data"} )
+    start, limit, sort, req = self.__request()
+    rpcClient = RPCClient( "Framework/ProxyManager" )
+    retVal = yield self.threadTask( rpcClient.getContents, req, sort, start, limit )
+    gLogger.info( "*!*!*!  RESULT: \n%s" % retVal )
+    if not retVal[ 'OK' ]:
+      self.finish( {"success":"false", "error":retVal["Message"]} )
+    svcData = retVal[ 'Value' ]
+    proxies = []
+    dnMap = {}
+    for record in svcData[ 'Records' ]:
+      proxies.append( { 'proxyid': "%s@%s" % ( record[1], record[2] ),
+                                  'UserName' : str( record[0] ),
+                                  'UserDN' : record[1],
+                                  'UserGroup' : record[2],
+                                  'ExpirationTime' : str( record[3] ),
+                                  'PersistentFlag' : str( record[4] ) } )
+    timestamp = Time.dateTime().strftime( "%Y-%m-%d %H:%M [UTC]" )
+    data = {"success":"true", "result":proxies, "total":svcData[ 'TotalRecords' ], "date":timestamp}
+    self.finish( data )
+
+  def __humanize_time( self, sec = False ):
+    """
+    Converts number of seconds to human readble values. Max return value is 
+    "More then a year" year and min value is "One day"
+    """
+    if not sec:
+      return "Time span is not specified"
+    try:
+      sec = int( sec )
+    except:
+      return "Value from CS is not integer"
+    month, week = divmod( sec, 2592000 )
+    if month > 0:
+      if month > 12:
+        return "More then a year"
+      elif month > 1:
+        return str( month ) + " months"
+      else:
+        return "One month"
+    week, day = divmod( sec, 604800 )
+    if week > 0:
+      if week == 1:
+        return "One week"
+      else:
+        return str( week ) + " weeks"
+    day, hours = divmod( sec, 86400 )
+    if day > 0:
+      if day == 1:
+        return "One day"
+      else:
+        return str( day ) + " days"
+  
+  def __request( self ):
+    gLogger.info( "!!!  PARAMS: ", str( self.request.arguments ) )
+    req = {}
+    try:
+      start = int( self.request.arguments[ 'start' ] )
+    except:
+      start = 0
+    try:
+      limit = int( self.request.arguments[ 'limit' ] )
+    except:
+      limit = 25
+    try:
+      sortDirection = str( self.request.arguments[ 'sortDirection' ] ).strip()
+    except:
+      sortDirection = "ASC"
+    try:
+      sortField = str( self.request.arguments[ 'sortField' ] ).strip()
+    except:
+      sortField = "UserName"
+    sort = [[sortField, sortDirection]]
+    gLogger.info( "!!!  S O R T : ", sort )
+    
+    if "username" in self.request.arguments:
+      users = list( json.loads( self.request.arguments[ 'username' ][-1] ) )
+      if len( users ) > 0:
+        req['UserName'] = users
+        
+    if "usergroup" in self.request.arguments:
+      usersgroup = list( json.loads( self.request.arguments[ 'usergroup' ][-1] ) )
+      if len( usersgroup ) > 0:
+        req['UserGroup'] = usersgroup
+    
+    if "usersgroup" in self.request.arguments and len( self.request.arguments["persistent"] ) > 0:
+      if str( self.request.arguments["persistent"] ) in ["True", "False"]:
+        req["PersistentFlag"] = str( self.request.arguments["persistent"] )
+    before = False
+    after = False
+    if self.request.arguments.has_key( "expiredBefore" ) and len( self.request.arguments["expiredBefore"] ) > 0:
+      try:
+        before = int( self.request.arguments["expiredBefore"] )
+      except:
+        pass
+    if self.request.arguments.has_key( "expiredAfter" ) and len( self.request.arguments["expiredAfter"] ) > 0:
+      try:
+        after = int( self.request.arguments["expiredAfter"] )
+      except:
+        pass
+    if before and after:
+      if before > after:
+        req["beforeDate"] = before      
+        req["afterDate"] = after
+    else:
+      if before:
+        req["beforeDate"] = before
+      if after:
+        req["afterDate"] = after
+    gLogger.always( "REQUEST:", req )
+    return ( start, limit, sort, req )
---
+++ WebApp/static/DIRAC/ProxyManager/classes/ProxyManager.js
@@ -0,0 +1,199 @@
+/*******************************************************************************
+ * It is the transformation monitor class.
+ */
+Ext.define('DIRAC.ProxyManager.classes.ProxyManager', {
+      extend : 'Ext.dirac.core.Module',
+      requires : ['Ext.panel.Panel', 'Ext.panel.Panel', 'Ext.dirac.utils.DiracBoxSelect', 'Ext.dirac.utils.DiracToolButton', 'DIRAC.TransformationMonitor.classes.GridPanel', "Ext.form.field.TextArea", "Ext.dirac.utils.DiracGridPanel", 'Ext.dirac.utils.DiracIdListButton',
+          'Ext.dirac.utils.DiracPageSizeCombo', 'Ext.dirac.utils.DiracPagingToolbar', 'Ext.dirac.utils.DiracJsonStore', 'Ext.dirac.utils.DiracAjaxProxy', 'Ext.dirac.utils.DiracApplicationContextMenu', 'Ext.dirac.utils.DiracBaseSelector'],
+
+      loadState : function(data) {
+        var me = this;
+
+        me.grid.loadState(data);
+
+        me.leftPanel.loadState(data);
+
+        if (data.leftPanelCollapsed) {
+
+          me.leftPanel.collapse();
+
+        }
+
+      },
+
+      getStateData : function() {
+        var me = this;
+        var oStates = {};
+
+        oStates = {
+          grid : me.grid.getStateData(),
+          leftMenu : me.leftPanel.getStateData()
+        };
+
+        oStates.leftPanelCollapsed = me.leftPanel.collapsed;
+
+        return oStates;
+      },
+      dataFields : [{
+            name : 'UserName'
+          }, {
+            name : 'UserDN'
+          }, {
+            name : 'UserGroup'
+          }, {
+            name : 'ExpirationTime',
+            type : 'date',
+            dateFormat : 'Y-m-d H:i:s'
+          }, {
+            name : 'PersistentFlag'
+          }],
+
+      initComponent : function() {
+        var me = this;
+
+        GLOBAL.APP.CF.log("debug", "create the widget(initComponent)...");
+
+        me.launcher.title = "Proxy Manager";
+        me.launcher.maximized = false;
+
+        if (GLOBAL.VIEW_ID == "desktop") {
+
+          var oDimensions = GLOBAL.APP.MAIN_VIEW.getViewMainDimensions();
+
+          me.launcher.width = oDimensions[0];
+          me.launcher.height = oDimensions[1];
+
+          me.launcher.x = 0;
+          me.launcher.y = 0;
+
+        }
+
+        Ext.apply(me, {
+              layout : 'border',
+              bodyBorder : false,
+              defaults : {
+                collapsible : true,
+                split : true
+              }
+            });
+
+        me.callParent(arguments);
+
+      },
+      buildUI : function() {
+
+        var me = this;
+
+        GLOBAL.APP.CF.log("debug", "create the widget...(buildUI)");
+
+        var selectors = {
+          username : "User",
+          usergroup : "Group"
+        };
+
+        var map = [["username", "username"], ["usergroup", "usergroup"]];
+
+        me.leftPanel = new Ext.create('Ext.dirac.utils.DiracBaseSelector', {
+              scope : me,
+              cmbSelectors : selectors,
+              datamap : map,
+              hasTimeSearchPanel : false,
+              url : "ProxyManager/getSelectionData"
+            });
+
+        /*
+         * -----------------------------------------------------------------------------------------------------------
+         * DEFINITION OF THE GRID
+         * -----------------------------------------------------------------------------------------------------------
+         */
+        var oProxy = Ext.create('Ext.dirac.utils.DiracAjaxProxy', {
+              url : GLOBAL.BASE_URL + me.applicationName + "/getProxyManagerData"
+            });
+
+        me.diffValues = {};
+        me.dataStore = Ext.create("Ext.dirac.utils.DiracJsonStore", {
+              proxy : oProxy,
+              fields : me.dataFields,
+              groupField : 'UserName',
+              scope : me
+            });
+
+        var pagingToolbar = null;
+
+        var toolButtons = {
+          'Public' : [{
+                "text" : "Delete",
+                "handler" : me.__deleteProxyes,
+                "properties" : {
+                  tooltip : 'Click to delete the selected proxies!'
+                }
+              }]
+        };
+
+        pagingToolbar = Ext.create("Ext.dirac.utils.DiracPagingToolbar", {
+              toolButtons : toolButtons,
+              store : me.dataStore,
+              scope : me
+            });
+
+        var oColumns = {
+          "checkBox" : {
+            "dataIndex" : "proxyid"
+          },
+          "User" : {
+            "dataIndex" : "UserName",
+            "properties" : {
+              width : 100,
+              sortable : true
+            }
+          },
+          "DN" : {
+            "dataIndex" : "UserDN",
+            "properties" : {
+              width : 350,
+              sortable : true
+            }
+          },
+          "Group" : {
+            "dataIndex" : "UserGroup",
+            "properties" : {
+              width : 100,
+              sortable : true
+            }
+          },
+          "Expiration date (UTC)" : {
+            "dataIndex" : "ExpirationTime",
+            "properties" : {
+              width : 150,
+              sortable : true
+            },
+            "renderer" : Ext.util.Format.dateRenderer('Y-m-j H:i')
+          },
+          "Persistent" : {
+            "dataIndex" : "PersistentFlag",
+            "properties" : {
+              width : 100,
+              sortable : true
+            }
+          }
+        };
+
+        me.grid = Ext.create('Ext.dirac.utils.DiracGridPanel', {
+              store : me.dataStore,
+              features : [{
+                    ftype : 'grouping'
+                  }],
+              oColumns : oColumns,
+              pagingToolbar : pagingToolbar,
+              scope : me
+            });
+
+        me.leftPanel.setGrid(me.grid);
+
+        me.add([me.leftPanel, me.grid]);
+
+      },
+      __deleteProxyes : function() {
+        alert("Not implemented!!!");
+      }
+    });
