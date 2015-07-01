09c86dc96351079ae6c5e5693043549df5d8c070
Sbalbp <sbalbp@gmail.com>
2015-07-01 14:22:49 +0200

    Added ComponentHistory app
    This app allows to search and filter the history of installations of
    components made on different hosts, similar to the 'showInstallations'
    command exposed by the DIRAC CLI.

---
+++ WebApp/handler/ComponentHistoryHandler.py
@@ -0,0 +1,132 @@
+from WebAppDIRAC.Lib.WebHandler import WebHandler, WErr, WOK, asyncGen
+from DIRAC import gConfig, S_OK, S_ERROR, gLogger
+from DIRAC.Core.Utilities import Time
+from DIRAC.FrameworkSystem.Client.ComponentMonitoringClient import ComponentMonitoringClient
+import json
+import datetime
+
+class ComponentHistoryHandler( WebHandler ):
+
+  AUTH_PROPS = 'all'
+
+  @asyncGen
+  def web_getJobData( self ):
+
+    req = self.__request()
+
+    client = ComponentMonitoringClient()
+    result = client.getInstallations( req[ 'installation' ], req[ 'component' ], req[ 'host' ], True )
+    if result[ 'OK' ]:
+      values = []
+      installations = result[ 'Value' ]
+      for i in range( self.pageNumber, self.pageNumber + self.numberOfInstallations ):
+        if len( installations ) > i:
+          installation = installations[ i ]
+        else:
+          break
+        uninstalled = '-'
+        if installation[ 'UnInstallationTime' ]:
+          uninstalled = installation[ 'UnInstallationTime' ].strftime( '%Y-%m-%d %H:%M' )
+        values.append( { 'Name': installation[ 'Instance' ], \
+                         'Module': installation[ 'Component' ][ 'Module' ], \
+                         'Host': installation[ 'Host' ][ 'HostName' ], \
+                         'System': installation[ 'Component' ][ 'System' ], \
+                         'Type': installation[ 'Component' ][ 'Type' ], \
+                         'Installed': installation[ 'InstallationTime' ].strftime( '%Y-%m-%d %H:%M' ), \
+                         'Uninstalled': uninstalled } )
+      timestamp = Time.dateTime().strftime( '%Y-%m-%d %H:%M [UTC]' )
+      total = len( installations )
+      callback = { 'success' : 'true', 'result' : values,
+                'total' : total, 'date' : timestamp }
+    else:
+      callback = { 'success' : 'false' , 'error' : result[ 'Message' ] }
+    self.finish( callback )
+
+  @asyncGen
+  def web_getSelectionData( self ):
+
+    req = self.__request()
+
+    data = {}
+    userData = self.getSessionData()
+
+    setup = userData[ 'setup' ].split( '-' )[-1]
+    systemList = []
+    system = None
+
+    fields = [ 'name', 'host', 'module', 'system', 'type' ]
+
+    client = ComponentMonitoringClient()
+    result = client.getInstallations( {}, {}, {}, True )
+    if result[ 'OK' ]:
+      for field in fields:
+        data[ field ] = set()
+      for installation in result[ 'Value' ]:
+        data[ 'name' ].add( installation[ 'Instance' ] )
+        data[ 'host' ].add( installation[ 'Host' ][ 'HostName' ] )
+        data[ 'module' ].add( installation[ 'Component' ][ 'Module' ] )
+        data[ 'system' ].add( installation[ 'Component' ][ 'System' ] )
+        data[ 'type' ].add( installation[ 'Component' ][ 'Type' ] )
+
+      for field in fields:
+        data[ field ] = list( data[ field ] )
+        data[ field ].sort()
+        data[ field ] = map( lambda x : [x] , data[ field ] )
+    else:
+      data = { 'success' : 'false' , 'error' : result[ 'Message' ] }
+
+    self.finish( data )
+
+  def __request( self ):
+    req = { 'installation': {}, 'component': {}, 'host': {} }
+
+    # Figure out what page we are at and how many results we are displaying
+    if 'limit' in self.request.arguments:
+      self.numberOfInstallations = int( self.request.arguments[ 'limit' ][-1] )
+      if 'start' in self.request.arguments:
+        self.pageNumber = int( self.request.arguments[ 'start' ][-1] )
+      else:
+        self.pageNumber = 0
+    else:
+      self.numberOfInstallations = 25
+      self.pageNumber = 0
+
+    # Check every possible selector
+
+    if 'name' in self.request.arguments:
+      print self.request.arguments[ 'name' ][-1]
+      req[ 'installation' ][ 'Instance' ] = list( json.loads( self.request.arguments[ 'name' ][-1] ) )
+
+    if 'host' in self.request.arguments:
+      print self.request.arguments[ 'host' ][-1]
+      req[ 'host' ][ 'HostName' ] = list( json.loads( self.request.arguments[ 'host' ][-1] ) )
+
+    if 'system' in self.request.arguments:
+      print self.request.arguments[ 'system' ][-1]
+      req[ 'component' ][ 'System' ] = list( json.loads( self.request.arguments[ 'system' ][-1] ) )
+
+    if 'module' in self.request.arguments:
+      print self.request.arguments[ 'module' ][-1]
+      req[ 'component' ][ 'Module' ] = list( json.loads( self.request.arguments[ 'module' ][-1] ) )
+
+    if 'type' in self.request.arguments:
+      print self.request.arguments[ 'type' ][-1]
+      req[ 'component' ][ 'Type' ] = list( json.loads( self.request.arguments[ 'type' ][-1] ) )
+
+    if 'startDate' in self.request.arguments and len( self.request.arguments[ 'startDate' ][0] ) > 0:
+      if len( self.request.arguments[ 'startTime' ][0] ) > 0:
+        time = self.request.arguments[ 'startTime' ][0]
+      else:
+        time = '00:00'
+      date = datetime.datetime.strptime( '%s-%s' % ( self.request.arguments[ 'startDate' ][0], time ), '%Y-%m-%d-%H:%M' )
+      req[ 'installation' ][ 'InstallationTime.bigger' ] = date
+
+    if 'endDate' in self.request.arguments and len( self.request.arguments[ 'endDate' ][0] ) > 0:
+      if len( self.request.arguments[ 'endTime' ][0] ) > 0:
+        time = self.request.arguments[ 'endTime' ][0]
+      else:
+        time = '00:00'
+      date = datetime.datetime.strptime( '%s-%s' % ( self.request.arguments[ 'endDate' ][0], time ), '%Y-%m-%d-%H:%M' )
+      req[ 'installation' ][ 'UnInstallationTime.bigger' ] = date
+
+    return req
---
+++ WebApp/static/DIRAC/ComponentHistory/classes/ComponentHistory.js
@@ -0,0 +1,185 @@
+Ext.define('DIRAC.ComponentHistory.classes.ComponentHistory', {
+  extend : 'Ext.dirac.core.Module',
+
+  requires :
+    ["Ext.dirac.utils.DiracToolButton", "Ext.dirac.utils.DiracGridPanel", "Ext.dirac.utils.DiracPagingToolbar",
+     "Ext.dirac.utils.DiracApplicationContextMenu","Ext.dirac.utils.DiracBaseSelector","Ext.dirac.utils.DiracAjaxProxy"],
+
+     loadState : function(data) {
+
+       var me = this;
+
+       me.grid.loadState(data);
+
+       me.leftPanel.loadState(data);
+
+       if (data.leftPanelCollapsed) {
+
+         if (data.leftPanelCollapsed)
+           me.leftPanel.collapse();
+
+       }
+     },
+
+     getStateData : function() {
+
+       var me = this;
+       var oReturn = {};
+
+       // data for grid columns
+       oReturn.grid = me.grid.getStateData();
+       // show/hide for selectors and their selected data (including NOT
+       // button)
+       oReturn.leftMenu = me.leftPanel.getStateData();
+
+       oReturn.leftPanelCollapsed = me.leftPanel.collapsed;
+
+       return oReturn;
+
+     },
+     dataFields : [
+       { name : 'Name' },
+       { name : 'Module' },
+       { name : 'Host' },
+       { name : 'System' },
+       { name : 'Type' },
+       { name : 'Installed' },
+       { name : 'Uninstalled' }
+     ],
+
+
+     initComponent : function() {
+
+       var me = this;
+
+       if (GLOBAL.VIEW_ID == "desktop") {
+
+         me.launcher.title = "Component history";
+         me.launcher.maximized = false;
+
+         var oDimensions = GLOBAL.APP.MAIN_VIEW.getViewMainDimensions();
+
+         me.launcher.width = oDimensions[0];
+         me.launcher.height = oDimensions[1] - GLOBAL.APP.MAIN_VIEW.taskbar.getHeight();
+
+         me.launcher.x = 0;
+         me.launcher.y = 0;
+
+       }
+
+       if (GLOBAL.VIEW_ID == "tabs") {
+
+         me.launcher.title = "Component history";
+         me.launcher.maximized = false;
+
+         var oDimensions = GLOBAL.APP.MAIN_VIEW.getViewMainDimensions();
+
+         me.launcher.width = oDimensions[0];
+         me.launcher.height = oDimensions[1] - GLOBAL.APP.MAIN_VIEW.taskbar.getHeight();
+
+         me.launcher.x = 0;
+         me.launcher.y = 0;
+
+       }
+
+       Ext.apply(me, {
+         layout : 'border',
+         bodyBorder : false,
+         defaults : {
+           collapsible : true,
+           split : true
+         }
+       });
+
+       me.callParent(arguments);
+
+     },
+
+     buildUI : function() {
+
+       var me = this;
+
+       /*
+        * -----------------------------------------------------------------------------------------------------------
+        * DEFINITION OF THE LEFT PANEL
+        * -----------------------------------------------------------------------------------------------------------
+        */
+
+       var selectors = {
+           name : "Name",
+           host : "Host",
+           system : "System",
+           module : "Module",
+           type : "Type"
+       };
+
+       var textFields = {}
+
+       var map = [ [ "name", "name" ], [ "host", "host" ], [ "system", "system" ], [ "module", "module" ], [ "type", "type" ] ];
+
+       me.leftPanel = Ext.create('Ext.dirac.utils.DiracBaseSelector',{
+         scope : me,
+         cmbSelectors : selectors,
+         textFields : textFields,
+         datamap : map,
+         hasTimeSearchPanel : true,
+         url : "ComponentHistory/getSelectionData"
+       });
+
+       /*
+        * -----------------------------------------------------------------------------------------------------------
+        * DEFINITION OF THE GRID
+        * -----------------------------------------------------------------------------------------------------------
+        */
+
+       var oProxy = Ext.create('Ext.dirac.utils.DiracAjaxProxy',{
+         url : GLOBAL.BASE_URL + 'ComponentHistory/getJobData'
+       });
+
+       me.dataStore = Ext.create("Ext.dirac.utils.DiracJsonStore",{
+         autoLoad : false,
+         proxy : oProxy,
+         fields : me.dataFields,
+         scope : me,
+         remoteSort : false,
+         autoLoad : true});
+
+       var pagingToolbar = {};
+
+       var toolButtons = {
+           'Visible':[
+                      {"text":"", "handler":me.__executeAction, "arguments":["example", ""],"properties":{tooltip : "Example", iconCls : "dirac-icon-reschedule"}}
+                      ]
+       };
+
+       pagingToolbar = Ext.create("Ext.dirac.utils.DiracPagingToolbar",{
+         store : me.dataStore,
+         scope : me
+       });
+
+       var oColumns = {
+           "Name" : { "dataIndex" : "Name", "properties" : { "width" : 180, "align" : "center" } },
+           "Module" : { "dataIndex" : "Module", "properties" : { "width" : 180, "align" : "center" } },
+           "Host" : { "dataIndex" : "Host", "properties" : { "width" : 180, "align" : "center" } },
+           "System" : { "dataIndex" : "System", "properties" : { "width" : 180, "align" : "center" } },
+           "Type" : { "dataIndex" : "Type", "properties" : { "width" : 100, "align" : "center" } },
+           "Installed" : { "dataIndex" : "Installed", "properties" : { "width" : 120, "align" : "center" } },
+           "Uninstalled" : { "dataIndex" : "Uninstalled", "properties" : { "width" : 120, "align" : "center" } }
+       };
+
+       me.grid = Ext.create('Ext.dirac.utils.DiracGridPanel', {
+         store : me.dataStore,
+         oColumns : oColumns,
+         pagingToolbar : pagingToolbar,
+         scope : me
+       });
+
+       me.leftPanel.setGrid(me.grid);
+
+       me.add([ me.leftPanel, me.grid ]);
+     },
+     __executeAction : function(action){
+       var me = this;
+       GLOBAL.APP.CF.alert(action+" button pressed","info");
+     }
+});
