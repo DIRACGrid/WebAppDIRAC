9aea07ae3d314b3da7ef26659db901ac0dffdb5d
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2015-09-13 14:05:51 +0200

    Wrap up a long line

--- WebApp/static/DIRAC/FileCatalog/classes/FileCatalog.js
+++ WebApp/static/DIRAC/FileCatalog/classes/FileCatalog.js
@@ -1,7 +1,11 @@
 Ext.define('DIRAC.FileCatalog.classes.FileCatalog', {
       extend : 'Ext.dirac.core.Module',
-      requires : ['Ext.util.*', 'Ext.panel.Panel', "Ext.form.field.Text", "Ext.button.Button", "Ext.menu.Menu", "Ext.form.field.ComboBox", "Ext.layout.*", "Ext.form.field.TextArea", "Ext.form.field.Checkbox", "Ext.form.FieldSet", "Ext.Button", "Ext.util.*",
-          "Ext.toolbar.Toolbar", "Ext.data.Record", "Ext.tree.Panel", "Ext.data.TreeStore", "Ext.data.NodeInterface", 'Ext.form.field.TextArea', 'Ext.Array', 'Ext.grid.Panel', 'Ext.form.field.Text', 'Ext.grid.feature.Grouping', 'Ext.tree.Panel', 'Ext.data.TreeStore'],
+      requires : ['Ext.util.*', 'Ext.panel.Panel', "Ext.form.field.Text", "Ext.button.Button", "Ext.menu.Menu",
+                  "Ext.form.field.ComboBox", "Ext.layout.*", "Ext.form.field.TextArea", "Ext.form.field.Checkbox",
+                  "Ext.form.FieldSet", "Ext.Button", "Ext.util.*", "Ext.toolbar.Toolbar", "Ext.data.Record",
+                  "Ext.tree.Panel", "Ext.data.TreeStore", "Ext.data.NodeInterface", 'Ext.form.field.TextArea',
+                  'Ext.Array', 'Ext.grid.Panel', 'Ext.form.field.Text', 'Ext.grid.feature.Grouping',
+                  'Ext.tree.Panel', 'Ext.data.TreeStore'],
 
       loadState : function(oData) {
 
