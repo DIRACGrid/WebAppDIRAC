fe3f60bed4d5a426b2b36abd34d41acd4444c7dc
Zoltan Mathe <zoltan.mathe@cern.ch>
2016-10-20 12:03:53 +0200

    allow to upload file to the web server

--- WebApp/handler/RootHandler.py
+++ WebApp/handler/RootHandler.py
@@ -15,6 +15,26 @@ class RootHandler(WebHandler):
   AUTH_PROPS = "all"
   LOCATION = "/"
 
+  def web_upload( self ):
+     
+     if 'filename' not in self.request.arguments:
+       raise WErr( 400, "Please provide a file name!" )
+     data = self.request.arguments.get( "data", "" )[0]
+     filename = self.request.arguments.get( "filename", "" )[0]
+     
+     if re.match( "(?!\.)^[\w\d_\.]*$", filename ):
+       filepath = "%s/webRoot/www/pilot/%s" % ( rootPath, filename )
+     else:
+       raise WErr( 400, "Please provide a valid file name!" )
+     
+     try:
+       out_file = open( filepath, 'w' )      
+       out_file.write( data )
+       out_file.close()
+     except Exception as e:
+       raise WErr( 400, "Cannot create the file: %s" % e )
+     self.finish( 'File has created' )
+ 
   def web_changeGroup( self ):
     try:
       to = self.request.arguments[ 'to' ][-1]
