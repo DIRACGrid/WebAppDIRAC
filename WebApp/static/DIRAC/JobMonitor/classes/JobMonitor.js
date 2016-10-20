7877548b21935bc9f7c94e08acf40744394bd614
Zoltan Mathe <zoltan.mathe@cern.ch>
2016-10-20 12:35:52 +0200

    remove google plot from the JobMonitor application

--- WebApp/static/DIRAC/JobMonitor/classes/JobMonitor.js
+++ WebApp/static/DIRAC/JobMonitor/classes/JobMonitor.js
@@ -706,43 +706,57 @@ Ext.define('DIRAC.JobMonitor.classes.JobMonitor', {
               scope : me
 
             });
-
+         
         me.btnShowPlotAsPng = new Ext.Button({
 
               margin : 0,
               iconCls : "dirac-icon-save",
               handler : function() {
 
-                var sSvgElement = document.getElementById(me.id + "-statistics-plot").getElementsByTagName("svg")[0].parentNode.innerHTML;
+                var plot = me.statisticsPlotPanel.down("chart");
 
-                var iHeight = me.statisticsPlotPanel.getHeight();
+                var domEl = plot.getEl();
+                if (domEl && domEl.dom) {
 
-                var iWidth = me.statisticsPlotPanel.getWidth();
+                  var sSvgElement = domEl.dom.getElementsByTagName("svg")[0].parentNode.innerHTML;
 
-                var canvas = document.createElement('canvas');
-                canvas.setAttribute('width', iWidth);
-                canvas.setAttribute('height', iHeight);
+                  var iHeight = me.statisticsPlotPanel.getHeight();
 
-                var oContext = canvas.getContext("2d");
+                  var iWidth = me.statisticsPlotPanel.getWidth();
 
-                oContext.beginPath();
-                oContext.rect(0, 0, iWidth, iHeight);
-                oContext.fillStyle = "#FFFFFF";
-                oContext.fill();
+                  var canvas = document.createElement('canvas');
+                  canvas.setAttribute('width', iWidth);
+                  canvas.setAttribute('height', iHeight);
 
-                var oImage = new Image();
-                oImage.src = GLOBAL.ROOT_URL + 'static/core/img/wallpapers/dirac_jobmonitor_background.png';
+                  var oContext = canvas.getContext("2d");
 
-                oImage.onload = function() {
+                  oContext.beginPath();
+                  oContext.rect(0, 0, iWidth, iHeight);
+                  oContext.fillStyle = "#FFFFFF";
+                  oContext.fill();
 
-                  console.log([oImage.clientWidth, oImage.clientHeight]);
+                  var oImage = new Image();
+                  oImage.src = GLOBAL.ROOT_URL + 'static/core/img/wallpapers/dirac_jobmonitor_background.png';
 
-                  oContext.drawImage(oImage, 0, 0, iWidth, iHeight);
+                  oImage.onload = function() {
 
-                  oContext.drawSvg(sSvgElement, 0, 0);
+                    console.log([oImage.clientWidth, oImage.clientHeight]);
 
-                  var imgData = canvas.toDataURL("image/png");
-                  window.location = imgData.replace("image/png", "image/octet-stream");
+                    oContext.drawImage(oImage, 0, 0, iWidth, iHeight);
+
+                    oContext.drawSvg(sSvgElement, 0, 0);
+
+                    var imgData = canvas.toDataURL("image/png");
+                    window.location = imgData.replace("image/png", "image/octet-stream");
+
+                  }
+                } else {
+                  plot.save({
+                        type : 'image/png',
+                        src : GLOBAL.ROOT_URL + 'static/core/img/wallpapers/dirac_jobmonitor_background.png',
+                        backround : GLOBAL.ROOT_URL + 'static/core/img/wallpapers/dirac_jobmonitor_background.png',
+                        cls : GLOBAL.ROOT_URL + 'static/core/img/wallpapers/dirac_jobmonitor_background.png'
+                      });
 
                 }
 
@@ -752,11 +766,10 @@ Ext.define('DIRAC.JobMonitor.classes.JobMonitor', {
             });
 
         me.btnPlotSettings = new Ext.Button({
-
               margin : 0,
-              iconCls : "dirac-icon-pie",
+              iconCls : "dirac-icon-settings",
               handler : function() {
-
+                
                 me.formPlotSettings();
 
               },
@@ -891,18 +904,33 @@ Ext.define('DIRAC.JobMonitor.classes.JobMonitor', {
               scope : me
             })
 
+        me.plotSettings = {};
+        
+        me.plotSettings.plotTitle = {
+          type : 'text',
+          text : 'No data',
+          font : '18px Helvetica',
+          width : 100,
+          height : 30,
+          x : 40, // the sprite x position
+          y : 12
+          // the sprite y position
+        };
+        me.plotSettings.plotLegend = {
+            field : 'key',
+            position : 'right',
+            boxStrokeWidth : 2,
+            labelFont : '12px Helvetica',
+            padding : 10
+          };
         me.statisticsPlotPanel = new Ext.create('Ext.panel.Panel', {
-              region : 'center',
-              floatable : false,
-              layout : 'fit',
-              header : false,
-              items : [{
-                    html : "<div id='" + me.id + "-statistics-plot' style='width:100%;'></div>",
-                    xtype : "box",
-                    cls : 'jm-statistics-plot-background'
-                  }]
-            });
-
+          autoScroll : true,
+          region : 'center',
+          floatable : false,
+          layout : 'fit',
+          header : false
+          });
+        
         me.statisticsPlotPanel.onResize = function(width, height, oldWidth, oldHeight) {
 
           me.createPlotFromGridData(me.statisticsGridComboMain.getValue() + " :: " + me.statisticsGridCombo.getValue());
@@ -950,7 +978,7 @@ Ext.define('DIRAC.JobMonitor.classes.JobMonitor', {
               allowBlank : false,
               margin : 10,
               anchor : '100%',
-              value : me.plotSettings.backupSettings.title
+              value : me.plotSettings.plotTitle.text
 
             });
 
@@ -965,7 +993,7 @@ Ext.define('DIRAC.JobMonitor.classes.JobMonitor', {
               valueField : "value",
               anchor : "100%",
               margin : 10,
-              value : me.plotSettings.backupSettings.legend
+              value : me.plotSettings.plotLegend.legend
             });
 
         // button for saving the state
@@ -1096,59 +1124,57 @@ Ext.define('DIRAC.JobMonitor.classes.JobMonitor', {
       createPlotFromGridData : function(sTitle, sLegendPosition) {
 
         var me = this;
-
+        me.statisticsPlotPanel.removeAll();
         if (!sLegendPosition) {
-          if (("plotSettings" in me) && ("backupSettings" in me.plotSettings)) {
-            sLegendPosition = me.plotSettings.backupSettings.legend;
+          if (("plotSettings" in me) && ("plotLegend" in me.plotSettings)) {
+            sLegendPosition = me.plotSettings.plotLegend.legend;
           } else {
             sLegendPosition = "right";
           }
         }
 
-        var oStore = me.statisticsSelectionGrid.getStore();
-        var oData = [["Key", "Value"]];
-        var oColors = [];
-
-        for (var i = 0; i < oStore.getCount(); i++) {
-
-          oData.push([oStore.getAt(i).get("key"), oStore.getAt(i).get("value")]);
-          oColors.push(oStore.getAt(i).get("color"));
-
-        }
-
-        var data = google.visualization.arrayToDataTable(oData);
-
         var oNow = new Date();
 
-        var options = {
-          title : sTitle + " (" + oNow.toString() + ")",
-          legend : {
-            position : sLegendPosition
-          },
-          colors : oColors,
-          backgroundColor : "transparent",
-          chartArea : {
-            width : "80%",
-            height : "80%"
-          }
-        };
-
-        if (!("plotSettings" in me))
-          me.plotSettings = {};
-
-        me.plotSettings.backupSettings = {
-          "title" : sTitle,
-          "legend" : sLegendPosition
-        };
-
-        var iHeight = me.statisticsPlotPanel.getHeight() - 20;
-        document.getElementById(me.id + "-statistics-plot").style.height = "" + iHeight + "px";
-
-        var iWidth = me.statisticsPlotPanel.getWidth() - 20;
-        document.getElementById(me.id + "-statistics-plot").style.width = "" + iWidth + "px";
-
-        var chart = new google.visualization.PieChart(document.getElementById(me.id + "-statistics-plot"));
-        chart.draw(data, options);
+        me.plotSettings.plotTitle.text = sTitle + " (" + oNow.toString() + ")"; 
+        me.plotSettings.plotLegend.position = sLegendPosition;
+        
+        var plot = Ext.create("Ext.chart.Chart", {
+              height : 410,
+              padding : '10 0 0 0',
+              cls : 'jm-statistics-plot-background',
+              animate : true,
+              shadow : false,
+              store : me.statisticsSelectionGrid.getStore(),
+              insetPadding : 40,
+              legend : me.plotSettings.plotLegend,
+              theme: 'Base:gradients',
+              items : [me.plotSettings.plotTitle],
+              series : [{
+                    type : 'pie',
+                    cls : 'jm-statistics-plot-background',
+                    angleField : 'value',
+                    label : {
+                      field : 'key',
+                      display : 'outside',
+                      calloutLine : true
+                    },
+                    showInLegend : true,
+                    highlight : {
+                      segment : {
+                        margin : 20
+                      }
+                    },
+                    tips : {
+                      width: 140,
+                      height: 28,
+                      trackMouse : true,
+                      renderer : function(storeItem, item) {
+                        this.setTitle(storeItem.get('key') + ': ' + storeItem.get('value'));
+                      }
+                    }
+                  }]
+            });
+        me.statisticsPlotPanel.add(plot);
 
       },
       __oprJobAction : function(oAction, useSelectedJobId) {
@@ -1302,7 +1328,8 @@ Ext.define('DIRAC.JobMonitor.classes.JobMonitor', {
                   } else if (oDataKind == "getLogURL") {
 
                     me.getContainer().oprShowInNewTab(jsonData["result"], 'Log file for JobId:' + oId);
-                    //me.getContainer().oprPrepareAndShowWindowHTML(jsonData["result"], 'Log file for JobId:' + oId);
+                    // me.getContainer().oprPrepareAndShowWindowHTML(jsonData["result"],
+                    // 'Log file for JobId:' + oId);
 
                   } else if (oDataKind == "getPending") {
                     var data = [];
