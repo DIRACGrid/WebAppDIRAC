5bacf0e92327fbe201a7b01cc6465f165d330d59
dirac <dirac@ce-emi.bitp.kiev.ua>
2018-11-21 16:17:06 +0200

    Make Welcome page configurable

--- WebApp/static/core/js/utils/WelcomeWindow.js
+++ WebApp/static/core/js/utils/WelcomeWindow.js
@@ -4,21 +4,39 @@
  */
 Ext.define('Ext.dirac.utils.WelcomeWindow', {
 	constructor : function() {
-        var usr = GLOBAL.APP.configData["user"]["username"];
-        if ((!Ext.util.Cookies.get('frstEntrUsrs')) || (Ext.util.Cookies.get('frstEntrUsrs').indexOf(usr) == -1)) {
-            Ext.get("app-dirac-welcome").show();
-            Ext.get("app-dirac-welcome-close").on('click',function(){Ext.get("app-dirac-welcome").hide()});
+        var usr = "visitor"
+        if (GLOBAL.APP.configData["user"]["username"]) {
+            usr = GLOBAL.APP.configData["user"]["username"]
+        }
+        var welcome = GLOBAL.WELCOME
+        var frstEntrUsrs = []
+        if (Ext.util.Cookies.get('frstEntrUsrs')) {
+            frstEntrUsrs = Ext.util.Cookies.get('frstEntrUsrs').split(",")
+        }
+        if ((frstEntrUsrs.indexOf(usr) === -1) && (welcome.show != "False")) {
+            frstEntrUsrs.push(usr)
+            Ext.util.Cookies.set('frstEntrUsrs',frstEntrUsrs)
+
+            var maindiv = document.createElement('div');
+            maindiv.id = "app-dirac-welcome"
+            maindiv.style = welcome.style
+            var textdiv = document.createElement('div')
+            textdiv.className = "app-dirac-welcome-text"
+            textdiv.innerHTML = '<h1>' + welcome.title + '</h1>' + welcome.text
+            // If not registred user
             if (!GLOBAL.APP.configData.user.username) {
-                Ext.get("app-dirac-welcome-visitor").show();
+                var addtextdiv = document.createElement('div')
+                addtextdiv.id = "app-dirac-welcome-visitor"
+                addtextdiv.innerHTML = '<h2>' + welcome.visitor_title + '</h2>' + welcome.visitor_text
+                textdiv.appendChild(addtextdiv)
             }
-        };
-        if (!Ext.util.Cookies.get('frstEntrUsrs')) {
-            Ext.util.Cookies.set('frstEntrUsrs',usr);
-        } else {
-            var frstEntrUsrs = [];
-            frstEntrUsrs.push(Ext.util.Cookies.get('frstEntrUsrs'),usr);
-            Ext.util.Cookies.set('frstEntrUsrs',frstEntrUsrs);
-        };
-        return(frstEntrUsrs);
+            var closebtn = document.createElement('button')
+            closebtn.id = "app-dirac-welcome-close"
+            closebtn.textContent = 'x'
+            closebtn.onclick = function(){maindiv.style.visibility = "hidden"}
+            maindiv.append(textdiv)
+            maindiv.append(closebtn)
+            document.body.appendChild(maindiv)
+        }
     }
 });
