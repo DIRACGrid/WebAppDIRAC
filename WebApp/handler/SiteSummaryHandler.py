cde46f1148148521401c5aec96e9e85fc3e2423e
TaykYoku <yokutayk@gmail.com>
2021-03-07 10:36:08 +0100

    add ExecutionSite key for DataOperation

--- WebApp/handler/SiteSummaryHandler.py
+++ WebApp/handler/SiteSummaryHandler.py
@@ -11,7 +11,8 @@ from WebAppDIRAC.Lib.WebHandler import asyncGen
 from WebAppDIRAC.WebApp.handler.ResourceSummaryHandler import ResourceSummaryHandler
 
 class SiteSummaryHandler(ResourceSummaryHandler):
-
+  """ SiteSummaryHandler 
+  """
   AUTH_PROPS = "all"
   ELEMENT_TYPE = 'Site'
 
@@ -251,7 +252,8 @@ class SiteSummaryHandler(ResourceSummaryHandler):
                   status=None):
 
     plotDict = {'condDict': {
-        'Site': [siteName],
+        # DIRAC.AccountingSystem.Client.Types.DataOperation class use 'ExecutionSite' key instead 'Site'
+        'ExecutionSite' if typeName == 'DataOperation' else 'Site': [siteName],
         'grouping': [grouping]
     },
         'extraArgs': {
