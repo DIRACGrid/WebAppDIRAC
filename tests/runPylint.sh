4439fafd87bd41d48bbbd287d70c9079c2d204fc
TaykYoku <yokutayk@gmail.com>
2021-04-14 18:39:07 +0200

    mv pytest.ini to tests

--- tests/runPylint.sh
+++ tests/runPylint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "${CHECK}" == "pylintPY3K"  ]]; then
-  find find * -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=.pylintrc3k --py3k --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
+  find src/WebAppDIRAC/* -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=tests/.pylintrc3k --py3k --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
 else
-  find find * -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=.pylintrc --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
+  find src/WebAppDIRAC/* -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=tests/.pylintrc --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
 fi
