dc23925a82e29400bec2b8edcf3b2ee63f4f4475
Andrii Lytovchenko <yokutayk@gmail.com>
2021-04-22 13:21:20 +0300

    Update tests/runPylint.sh
    Co-authored-by: Chris Burr <chrisburr@users.noreply.github.com>

--- tests/runPylint.sh
+++ tests/runPylint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "${CHECK}" == "pylintPY3K"  ]]; then
-  find src/WebAppDIRAC/* -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=tests/.pylintrc3k --py3k --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
+  find src/WebAppDIRAC/ -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=tests/.pylintrc3k --py3k --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
 else
   find src/WebAppDIRAC/* -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=.pylintrc --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
 fi
