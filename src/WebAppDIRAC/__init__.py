59875b4e480dc51e658e65a072a6562dda1b28b2
Chris Burr <christopher.burr@cern.ch>
2021-06-17 21:14:53 +0200

    Get version from Python metadata

--- src/WebAppDIRAC/__init__.py
+++ src/WebAppDIRAC/__init__.py
@@ -1,6 +1,5 @@
 """ WebAppDIRAC
 """
-
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
@@ -9,25 +8,33 @@ __RCSID__ = "$Id$"
 
 import os
 
+import six
+
+
 # Define Version
+if six.PY3:
+  from pkg_resources import get_distribution, DistributionNotFound
 
-majorVersion = 4
-minorVersion = 3
-patchLevel = 0
-preVersion = 5
+  try:
+    __version__ = get_distribution(__name__).version
+    version = __version__
+  except DistributionNotFound:
+    # package is not installed
+    version = "Unknown"
+else:
+  majorVersion = 4
+  minorVersion = 3
+  patchLevel = 0
+  preVersion = 5
 
-version = "v%sr%s" % (majorVersion, minorVersion)
-buildVersion = "v%dr%d" % (majorVersion, minorVersion)
-if patchLevel:
-  version = "%sp%s" % (version, patchLevel)
-  buildVersion = "%s build %s" % (buildVersion, patchLevel)
-if preVersion:
-  version = "%s-pre%s" % (version, preVersion)
-  buildVersion = "%s pre %s" % (buildVersion, preVersion)
-
-# Check of python version
-
-rootPath = os.path.realpath(os.path.dirname(__file__))
+  version = "v%sr%s" % (majorVersion, minorVersion)
+  buildVersion = "v%dr%d" % (majorVersion, minorVersion)
+  if patchLevel:
+    version = "%sp%s" % (version, patchLevel)
+    buildVersion = "%s build %s" % (buildVersion, patchLevel)
+  if preVersion:
+    version = "%s-pre%s" % (version, preVersion)
+    buildVersion = "%s pre %s" % (buildVersion, preVersion)
 
 
 def extension_metadata():
