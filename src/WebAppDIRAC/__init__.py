6ce7a6effae8e87de255aad9cd9c29b56a0b3600
TaykYoku <yokutayk@gmail.com>
2021-06-21 23:35:24 +0200

    v4r3-pre7, notes

--- src/WebAppDIRAC/__init__.py
+++ src/WebAppDIRAC/__init__.py
@@ -25,7 +25,7 @@ else:
   majorVersion = 4
   minorVersion = 3
   patchLevel = 0
-  preVersion = 5
+  preVersion = 7
 
   version = "v%sr%s" % (majorVersion, minorVersion)
   buildVersion = "v%dr%d" % (majorVersion, minorVersion)
