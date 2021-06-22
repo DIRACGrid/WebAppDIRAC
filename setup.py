06d2fc51932c8f361f32d1cc8b465a19507fe673
Chris Burr <christopher.burr@cern.ch>
2021-06-22 08:51:51 +0200

    Hackily compile webapp sources

--- setup.py
+++ setup.py
@@ -2,7 +2,26 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import subprocess
+from pathlib import Path
 from setuptools import setup
+
+
+# HACK: Compile the webapp sources
+subprocess.run(
+    [
+        "docker",
+        "run",
+        "--rm",
+        f"-v={Path(__file__).parent}:/shared",
+        "-w=/shared",
+        "diracgrid/dirac-distribution",
+        "/dirac-webapp-compile.py",
+        "-D=/shared/src",
+        "-n=WebAppDIRAC",
+    ],
+    check=True
+)
 
 # This is required to allow editable pip installs while using the declarative configuration (setup.cfg)
 setup()
