88b7d24a6a5a63cca2455ec3f255165ca9f82a4a
Chris Burr <christopher.burr@cern.ch>
2021-06-23 14:42:51 +0200

    Better way of compiling javascript sources in setuptools

--- setup.py
+++ setup.py
@@ -1,27 +1,65 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
+import os
 import subprocess
-from pathlib import Path
-from setuptools import setup
+
+# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
+# update it when the contents of directories change.
+if os.path.exists('MANIFEST'): os.remove('MANIFEST')
+
+from setuptools import Command, setup
+# Note: distutils must be imported after setuptools
+from distutils import log
+from setuptools.command.develop import develop as _develop
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
 
-# HACK: Compile the webapp sources
-subprocess.run(
-    [
-        "docker",
-        "run",
-        "--rm",
-        f"-v={Path(__file__).parent}:/shared",
-        "-w=/shared",
-        "diracgrid/dirac-distribution",
-        "/dirac-webapp-compile.py",
-        "-D=/shared/src",
-        "-n=WebAppDIRAC",
-    ],
-    check=True
-)
+class build_extjs_sources(Command):
+    user_options = []
 
-# This is required to allow editable pip installs while using the declarative configuration (setup.cfg)
-setup()
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def get_inputs(self):
+        return []
+
+    def get_outputs(self):
+        return []
+
+    def run(self):
+        path = os.path.abspath(os.getcwd())
+        cmd = [
+            "docker",
+            "run",
+            "--rm",
+            f"-v={path}:/shared",
+            "-w=/shared",
+            "diracgrid/dirac-distribution",
+            "/dirac-webapp-compile.py",
+            "-D=/shared/src",
+            "-n=WebAppDIRAC",
+        ]
+        log.info('> %r', cmd)
+        subprocess.check_call(cmd)
+
+
+class develop(_develop):
+    def run(self):
+        self.run_command("build_extjs_sources")
+        super().run()
+
+
+class bdist_wheel(_bdist_wheel):
+    def run(self):
+        self.run_command("build_extjs_sources")
+        super().run()
+
+
+cmdclass = {
+    "develop": develop,
+    "bdist_wheel": bdist_wheel,
+    "build_extjs_sources": build_extjs_sources,
+}
+
+setup(cmdclass=cmdclass)
