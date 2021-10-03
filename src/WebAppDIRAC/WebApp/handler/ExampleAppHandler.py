f1f05cfc9985f79af2691cecdf4375b75458d8fe
TaykYoku <yokutayk@gmail.com>
2021-10-03 22:16:49 +0200

    fix: add whitespaces

--- src/WebAppDIRAC/WebApp/handler/ExampleAppHandler.py
+++ src/WebAppDIRAC/WebApp/handler/ExampleAppHandler.py
@@ -8,14 +8,16 @@ class ExampleAppHandler(WebHandler):
   def web_getJobData(self):
     timestamp = Time.dateTime().strftime("%Y-%m-%d %H:%M [UTC]")
     total = 5
-    values = [{"ExampleId":1,"ExampleValue":"Zoli"},
-              {"ExampleId":2,"ExampleValue":'a'},
-              {"ExampleId":3,"ExampleValue":'aaaa'},
-              {"ExampleId":4,"ExampleValue":'bbbb'},
-              {"ExampleId":5,"ExampleValue":'adsd'}]
-    return {"success":"true", "result":values,
-            "total":total, "date":timestamp }
+    values = [{"ExampleId": 1, "ExampleValue": "Zoli"},
+              {"ExampleId": 2, "ExampleValue": 'a'},
+              {"ExampleId": 3, "ExampleValue": 'aaaa'},
+              {"ExampleId": 4, "ExampleValue": 'bbbb'},
+              {"ExampleId": 5, "ExampleValue": 'adsd'}]
+    return {"success": "true",
+            "result": values,
+            "total": total,
+            "date": timestamp }
 
   def web_getSelectionData(self):
-    return {"firstName":["A","C","D"],
-            "lastName":["wwww","dsds","sads"]}
+    return {"firstName": ["A", "C", "D"],
+            "lastName": ["wwww", "dsds", "sads"]}
