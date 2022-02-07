c9768616f882f2f021213c6c52b7a8113b881884
rupozzi <ruben.pozzi@cern.ch>
2022-02-07 10:49:08 +0100

    fix: Fixed bug nomenclature for PilotSubmissionMonitor

--- src/WebAppDIRAC/WebApp/static/DIRAC/Accounting/classes/Accounting.js
+++ src/WebAppDIRAC/WebApp/static/DIRAC/Accounting/classes/Accounting.js
@@ -110,7 +110,7 @@ Ext.define("DIRAC.Accounting.classes.Accounting", {
             ["status", "Status"],
           ],
         },
-        PilotMonitoring: {
+        PilotSubmissionMonitoring: {
           title: "Pilot Submission Monitoring",
           selectionConditions: [
             ["HostName", "HostName"],
@@ -137,7 +137,7 @@ Ext.define("DIRAC.Accounting.classes.Accounting", {
         ["WMSHistory", "WMS Monitoring"],
         ["ComponentMonitoring", "Component Monitoring"],
         ["RMSMonitoring", "RMS Monitoring"],
-        ["PilotMonitoring", "Pilot Submission Monitoring"],
+        ["PilotSubmissionMonitoring", "Pilot Submission Monitoring"],
       ],
     };
 
